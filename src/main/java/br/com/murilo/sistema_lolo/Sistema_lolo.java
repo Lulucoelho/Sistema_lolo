a32ae2eedd21c42ef5a4b78b72022638590efda1
Luisa Coelho <luisacoelho2005@gmail.com>
2023-10-02 20:58:50 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/murilo/sistema_lolo/Sistema_lolo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.murilo.sistema_lolo;
+
+/**
+ *
+ * @author 823158112
+ */
+public class Sistema_lolo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
